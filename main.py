94bfab74cc7976bc7379f6bdbb1fdf9c9db3a8d1
giannisPlymmenos <giannis.plymmenos@gmail.com>
2023-04-08 13:13:46 +0300

    Added highscore memory and optimized code.

--- main.py
+++ main.py
@@ -35,20 +35,21 @@ while game_is_on:
     # Detect collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
-        score.clear()
+
         snake.extend()
         score.increase_score()
+        score.update_scoreboard()
 
     # Detect collision with walls.
     if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
-        game_is_on = False
-        score.game_over()
+        score.reset()
+        snake.reset()
 
     # Detect collision with tail.
     for segment in snake.segments[1:]:
 
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            score.game_over()
+            score.reset()
+            snake.reset()
 
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -1,7 +1,8 @@
-
 # Import module
 from turtle import Turtle
 from turtle import Screen
+
+
 # Create class Scoreboard that inherits from the class Turtle.
 class Scoreboard(Turtle):
 
@@ -9,23 +10,29 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.penup()
         self.goto(0, 265)
         self.color("white")
         self.write(f"Score: {self.score}", align="center", font=("arial", 24, "normal"))
         self.hideturtle()
-
-    # Create game_over method.
-    def game_over(self):
-        self.goto(0, 0)
-        self.screen.clear()
-        self.screen.bgcolor("Black")
-        self.goto(0, 100)
-        self.write(f"GAME OVER", align="center", font=("arial", 24, "normal"))
-        self.goto(0, 0)
-        self.write(f"Final Score: {self.score}", align="center", font=("arial", 24, "normal"))
+        self.update_scoreboard()
 
     # Create increase_score method.
     def increase_score(self):
         self.score += 1
         self.write(f"Score: {self.score}", align="center", font=("arial", 24, "normal"))
+        self.update_scoreboard()
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode= "w") as data:
+                data.write(str(self.high_score))
+        self.score = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align="center", font=("arial", 24, "normal"))
